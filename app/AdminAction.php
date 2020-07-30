9998224693d95e9a8d4f6663f848a5ecf8744bcf
BarenAD <p.malashin@fterra.ru>
2020-07-30 10:23:13 +0700

    написал первоначальный сидер для действий администратора

---
+++ app/AdminAction.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AdminAction extends Model
+{
+    protected $table = 'admin_actions';
+
+    protected $fillable = [
+        'id', 'name', 'description', 'created_at', 'updated_at'
+    ];
+}
--- database/seeds/AdminActionsSeeder.php
+++ database/seeds/AdminActionsSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use App\AdminAction;
 
 class AdminActionsSeeder extends Seeder
 {
@@ -11,6 +13,63 @@ class AdminActionsSeeder extends Seeder
      */
     public function run()
     {
-        //
+        AdminAction::insert([
+            [
+                'id' => 1,
+                'name' => 'Управление администраторами',
+                'description' => 'Назначение ролей администрирования пользователям.',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ],
+            [
+                'id' => 2,
+                'name' => 'Управление ролями администратораторов',
+                'description' => 'Изменение ролей администраторов (добавление прав, удаление прав и редактирование роли)',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ],
+            [
+                'id' => 3,
+                'name' => 'Управление пользователями',
+                'description' => 'Редактирование и удаление пользователей',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ],
+            [
+                'id' => 4,
+                'name' => 'Глобальное управление товарами',
+                'description' => 'Глобальное управление товарами',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ],
+            [
+                'id' => 5,
+                'name' => 'Управление товарами',
+                'description' => 'Управление товарами в точках продаж.',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ],
+            [
+                'id' => 6,
+                'name' => 'Управление расписанием работы точек продаж',
+                'description' => 'Создание новых, удаление, редактирование.',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ],
+            [
+                'id' => 7,
+                'name' => 'Управление точками продаж',
+                'description' => 'Создание новых, удаление, редактирование, назначение расписаний',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ],
+            [
+                'id' => 8,
+                'name' => 'Управление категориями товаров',
+                'description' => 'Создание новых, удаление, редактирование',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ],
+        ]);
     }
 }
