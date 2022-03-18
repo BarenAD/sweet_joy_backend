10fd875bbbb1b6dd88647eab85613f9443450f17
BarenAD <baren1998@gmail.com>
2022-03-18 18:59:06 +0700

    add scoupe in Product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * Class Product
+ * @package App\Models
+ *
+ * @method Product withCategories(bool $with = false)
+ *
+ */
 class Product extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'products';
 
     protected $fillable = [
@@ -15,4 +26,17 @@ class Product extends Model
     protected $hidden = [
         'created_at', 'updated_at'
     ];
+
+    public function scopeWithCategories(Builder $query, bool $with = false)
+    {
+        if ($with) {
+            return $query->with('categories');
+        }
+        return $query;
+    }
+
+    public function categories()
+    {
+        return $this->hasMany(ProductCategory::class, 'product_id');
+    }
 }
